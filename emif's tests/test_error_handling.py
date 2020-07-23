e2caabded65e11de03a716b05dd78e3121dec6db
Emiliano Frigo <emiliano.frigo@followoz.com>
2020-07-23 18:02:33 -0300

    ok to proceed

--- emif's tests/test_error_handling.py
+++ emif's tests/test_error_handling.py
@@ -4,8 +4,11 @@ y = 0
 
 # try start
 try:
-    print(str(x / y))
+    print(x / y)
 except ZeroDivisionError as e:
     print('You cannot divide by zero, please change the value')
     print('Error thrown: ' + str(e))
+finally:
+    print('This is our cleanup code')
+
 
